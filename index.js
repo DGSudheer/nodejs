c6c547884b29be53c532c38b7a1181b4a2fcee40
D G Sudheer <dgsudheer@gmail.com>
2024-07-22 19:01:01 +0530

    process form data in GET and POST

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const app = express();
 
 const port = 5001;
 
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
 app.listen(port, () =>{
     console.log(`listening to port ${port}`);
 });
 
 app.get("/register", (req,res) =>{
-    console.log("GET request!");
-    res.send("GET request!");
+    console.log("GET request!");    
+    const {user, password} = req.query;
+//    console.log(req.params);
+    res.send(`GET request - Welcome ${user}!`);
 });
 
 app.post("/register", (req,res) =>{
     console.log("POST request!");
-    res.send("POST request!");
+    const {user, password} = req.body;
+    res.send(`POST request - Welcome ${user}!`);
 });
 
 
